1ca274c46acbdcc444a083e410d4054aeef9e30c
MalinEkanayake31 <malinekanayake22@gmail.com>
2024-07-01 20:11:34 +0530

    q1

---
+++ Practical_3/q1.scala
@@ -0,0 +1,18 @@
+import scala.io.StdIn
+
+object ReverseStringApp {
+  def main(args: Array[String]): Unit = {
+    println("Please enter a string:")
+
+    val inputString: String = StdIn.readLine()
+
+    val reversedString: String = reverseString(inputString)
+
+    println(s"Reversed string: $reversedString")
+  }
+
+  def reverseString(s: String): String = {
+    if (s.isEmpty) ""
+    else reverseString(s.tail) + s.head
+  }
+}
