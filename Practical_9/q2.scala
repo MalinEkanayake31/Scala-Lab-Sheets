c3015833d1d862f6a90815fd027666a9be5327b2
Malin Ashmitha Ekanayake <159604336+MalinEkanayake31@users.noreply.github.com>
2024-08-19 20:56:41 +0530

    practical 9

---
+++ Practical_9/q2.scala
@@ -0,0 +1,25 @@
+object q2{
+
+    def main(args: Array[String]): Unit = {
+
+        val isPositive: Int => Unit = num => num match{
+            case n if n <= 0 => println("Negative/Zero")
+            case _ => println("Positive")
+        }
+
+        val isEven: Int => Unit = num => num match {
+            case n if n%2==0 => println("Even Number")
+            case _ => println("Odd Number")
+        }
+
+        val manyFunctions: (Int => Unit , Int) => Unit = (thisFunction , para) => thisFunction(para)
+        print("Enter a Number: ")
+        val number = scala.io.StdIn.readInt()
+        
+
+        manyFunctions(isPositive , number)
+        manyFunctions(isEven , number)
+
+    }
+
+}
